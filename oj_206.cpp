6f01f6580ae15ddd5094fe68b758970ea35a21b7
wang.jing_202941 <wang.jing_202941@immomo.com>
2022-07-07 19:00:07 +0800

    206题目提交

--- oj_206.cpp
+++ oj_206.cpp
@@ -2,11 +2,11 @@
 // 按照0-18、19-35、36-60、61以上（含61）四个年龄段统计的患病人数占总患病人数的比例。
 
 #include <cstdio>
-
+#include <iostream>
+using namespace std;
 int main() {
 	int n, age, count1 = 0, count2 = 0, count3 = 0, count4 = 0;
 	scanf("%d", &n);
-
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &age);
 		if (age > 0 && age <= 18) {
@@ -22,9 +22,13 @@ int main() {
 			count4++;
 		}
 	}
-	printf("%.2lf\n", (double)count1);
-	printf("%.2lf\n", (double)count2);
-	printf("%.2lf\n", (double)count3);
-	printf("%.2lf\n", (double)count4);
+	printf("%.2lf", (double)count1 / n * 100);
+	cout << "%" << endl;
+	printf("%.2lf", (double)count2 / n * 100);
+	cout << "%" << endl;
+	printf("%.2lf", (double)count3 / n * 100);
+	cout << "%" << endl;
+	printf("%.2lf", (double)count4 / n * 100);
+	cout << "%";
 	return 0;
 }
